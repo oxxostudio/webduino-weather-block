17f225f4d613f22b880c509c1fb5a5e01fe7df0b
oxxo <oxxo.studio@gmail.com>
2018-05-21 09:48:25 +0800

    Init

---
+++ pm25.js
@@ -0,0 +1,14 @@
+{
+  "types": ["pm25"],
+  "category": "catPM25",
+  "scripts": [
+    "blockly/blocks.js",
+    "blockly/javascript.js"
+  ],
+  "dependencies": [
+    "pm25.js"    
+  ],
+  "msg": "blockly/msg",
+  "blocksMsg": "blockly/msg/blocks",
+  "toolbox": "blockly/toolbox.xml"
+}
